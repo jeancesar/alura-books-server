0bb29b24a975aa6974ae78193cd6d0ba2f420b6b
Jean Cesar <neo.jean@gmail.com>
2023-07-14 08:05:20 -0300

    Configuração para chamada da rota de livros

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const rotaLivro = require('./routes/livros');
+
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-    res.send('Olá mundo na Alura com Jean!');
-});
+app.use('/livros', rotaLivro);
 
 app.listen(port, () => {
     console.log(`Escutando a porta ${port}`);
